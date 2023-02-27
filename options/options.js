0da9c48552fde612555a501deeba5b3180d8d14d
Lior Neu-ner <lneuner@gmail.com>
2023-02-27 08:38:58 +0000

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "lior-test-39127",
   url: "https://lior-test-39127.botics.co",
   api: "https://lior-test-39127.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
